a76cad3cc0cb95570d9553e36124c5369eaa3bb1
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-09-13 15:59:51 -0700

    Add documentation to the Service class

--- lib/add_this_event/service.rb
+++ lib/add_this_event/service.rb
@@ -1,12 +1,28 @@
+##
+# Validates and provides a Service value for the AddThisEvent API
 module AddThisEvent
   class Service
     VALID_SERVICES = %w(appleical google outlook outlookcom yahoo)
 
+    ##
+    # @param [String, Symbol] service
+    #   The type of service to create the event for
+    #
+    # @return [AddThisEvent::Service]
+    #
+    # @api public
     def initialize(service)
       fail(ArgumentError, "service must set to #{VALID_SERVICES.join(', ')} or nil") unless valid_service?(service)
       @service = service || ''
     end
 
+    ##
+    # Returns a proper service value for the API
+    # either a string if a valid service was passed, or nil.
+    #
+    # @return [String]
+    #
+    # @api public
     def value
       return nil if @service.empty?
       @service.to_s
