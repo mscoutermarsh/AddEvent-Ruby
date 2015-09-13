156ba1768b83e772859948ca819786c34e5c2830
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-09-12 17:33:17 -0700

    Rubocop fixes

--- lib/add_this_event/service.rb
+++ lib/add_this_event/service.rb
@@ -1,20 +1,22 @@
-class AddThisEvent::Service
-  VALID_SERVICES = %w(appleical google outlook outlookcom yahoo)
+module AddThisEvent
+  class Service
+    VALID_SERVICES = %w(appleical google outlook outlookcom yahoo)
 
-  def initialize(service)
-    fail(ArgumentError, "service must set to #{VALID_SERVICES.join(', ')} or nil") unless valid_service?(service)
-    @service = service || ''
-  end
+    def initialize(service)
+      fail(ArgumentError, "service must set to #{VALID_SERVICES.join(', ')} or nil") unless valid_service?(service)
+      @service = service || ''
+    end
 
-  def value
-    return nil if @service.empty?
-    @service.to_s
-  end
+    def value
+      return nil if @service.empty?
+      @service.to_s
+    end
 
-  private
+    private
 
-  def valid_service?(service)
-    return true if service.nil? || service.empty?
-    VALID_SERVICES.include?(service.to_s)
+    def valid_service?(service)
+      return true if service.nil? || service.empty?
+      VALID_SERVICES.include?(service.to_s)
+    end
   end
 end
