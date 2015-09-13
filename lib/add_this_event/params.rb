b317022a0637e5d250338f6b99103eafa7799085
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-09-13 09:37:28 -0700

    Build out the rest of the params

--- lib/add_this_event/params.rb
+++ lib/add_this_event/params.rb
@@ -6,57 +6,16 @@ module AddThisEvent
   class Params
     extend Forwardable
 
+    # Available parameters. Full docs: https://addthisevent.com/api/
+    KEYS = %i(alarm all_day_event client date_format description end endext endtime location
+              organizer organizer_email reference service start startext starttime template timezone title)
+
     VALID_SERVICES = %w(outlook google appleical outlookcom yahoo)
     DATE_FORMAT = 'MM/DD/YYYY'
 
-    # Valid parameters. Full docs: https://addthisevent.com/api/
-    KEYS = %i(all_day_event
-              client
-              start
-              starttime
-              startext
-              date_format
-              service
-              title
-              organizer
-              organizer_email
-              description
-              location
-              timezone)
-
     attr_reader :params
 
-    delegate %i(title organizer description location organizer_email) => :params
-    # KEYS = %i{start starttime startext end endtime endext title
-    #           description location organizer all_day_event date_format
-    #           timezone reference service template alarm}
-
-    # ?client=
-    # &start=06%2F25%2F2015
-    # &starttime=09%3A00%3A00
-    # &startext=AM
-    # &end=06%2F25%2F2015
-    # &endtime=11%3A00%3A00
-    # &endext=AM
-    # &title=Headline+of+the+event
-    # &description=Description+of+the+event
-    # &location=Eiffel+Tower%2C+Champ+de+Mars%2C+5+Anatole+France+Avenue%2C+75007+Paris%2C+France
-    # &organizer=Your+Organization
-    # &organizer_email=your%40email%2Ecom
-    # &all_day_event=false
-    # &date_format=MM%2FDD%2FYYYY
-    # &timezone=Europe%2FParis
-    # &reference=iphone+app
-    # &service=appleical
-    # &template=lm2015e14o720j11
-    # &alarm=60#
-    #
-    # mandatory
-    # &start= 06/25/2015
-    # &end=
-    # &title=
-    # &date_format=
-    # &timezone=#
+    delegate %i(title organizer description location organizer_email template reference alarm) => :params
 
     def initialize(params)
       @params = OpenStruct.new(params)
@@ -78,14 +37,26 @@ module AddThisEvent
       params.starts_at.strftime('%m/%d/%Y')
     end
 
+    def end
+      params.ends_at.strftime('%m/%d/%Y')
+    end
+
     # Event start time. Mandatory. Format: "HH/MM/SS"
     def starttime
       params.starts_at.strftime('%I:%M:%S')
     end
 
+    def endtime
+      params.ends_at.strftime('%I:%M:%S')
+    end
+
     # AM or PM
     def startext
       params.starts_at.strftime('%p')
+    end
+
+    def endext
+      params.ends_at.strftime('%p')
     end
 
     def date_format
--- spec/add_this_event/params_spec.rb
+++ spec/add_this_event/params_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 describe AddThisEvent::Params do
   describe '#to_h' do
-    let(:title) { 'Event Title' }
     let(:starts_at) { DateTime.new(1988, 10, 06) }
-    let(:ends_at) { starts_at + 60 * 60 }
+    let(:ends_at) { starts_at }
 
     let(:params) do
-      { title: title,
+      { alarm: 60,
+        description: 'North End Team Lunch :happy:',
+        location: 'Hanover St, Boston, MA 02113',
+        organizer: 'Alexander Ovechkin',
+        organizer_email: 'coutermarsh.mike@gmail.com',
+        title: 'Team Lunch',
         client_id: 'fake_key',
         starts_at: starts_at,
         ends_at: ends_at,
@@ -26,18 +30,20 @@ describe AddThisEvent::Params do
       end.to raise_error ArgumentError
     end
 
-    it { is_expected.to include(start: '10/06/1988') }
     it { is_expected.to include(date_format: described_class::DATE_FORMAT) }
     it { is_expected.to include(service: 'appleical') }
+    it { is_expected.to include(start: '10/06/1988') }
     it { is_expected.to include(startext: 'AM') }
     it { is_expected.to include(starttime: '12:00:00') }
+    it { is_expected.to include(end: '10/06/1988') }
+    it { is_expected.to include(endext: 'AM') }
+    it { is_expected.to include(endtime: '12:00:00') }
     it { is_expected.to include(timezone: '+00:00') }
-    it { is_expected.to include(title: title) }
-    it { is_expected.to include(location: nil) }
-    it { is_expected.to include(organizer: nil) }
-    it { is_expected.to include(organizer_email: nil) }
+    it { is_expected.to include(title: params[:title]) }
+    it { is_expected.to include(location: params[:location]) }
+    it { is_expected.to include(organizer: params[:organizer]) }
+    it { is_expected.to include(organizer_email: params[:organizer_email]) }
     it { is_expected.to include(all_day_event: false) }
     it { is_expected.to include(client: 'fake_key') }
-
   end
 end
