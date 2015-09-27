bc358d6b061038a0c5db5159be02c6fcea7314f5
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-09-27 12:04:04 -0700

    Add documentation to ::Params

--- lib/add_this_event/params.rb
+++ lib/add_this_event/params.rb
@@ -2,25 +2,39 @@ require 'add_this_event/service'
 require 'forwardable'
 require 'ostruct'
 
+##
+# Takes in a hash of parameters and formats them for use with the AddThisEvent API
 module AddThisEvent
   class Params
     extend Forwardable
 
     # Available parameters. Full docs: https://addthisevent.com/api/
     KEYS = %i(alarm all_day_event client date_format description end endext endtime location
-              organizer organizer_email reference service start startext starttime template timezone title)
+              organizer organizer_email reference service start startext starttime template timezone title).freeze
 
-    VALID_SERVICES = %w(outlook google appleical outlookcom yahoo)
-    DATE_FORMAT = 'MM/DD/YYYY'
+    VALID_SERVICES = %w(outlook google appleical outlookcom yahoo).freeze
+    DATE_FORMAT = 'MM/DD/YYYY'.freeze
 
     attr_reader :params
 
     delegate %i(title organizer description location organizer_email template reference alarm) => :params
 
+    ##
+    # @param [Hash] params
+    #
+    # @return [AddThisEvent::Params]
+    #
+    # @api public
     def initialize(params)
       @params = OpenStruct.new(params)
     end
 
+    ##
+    # Returns a hash with only valid params for the AddThisEvent API
+    #
+    # @return [Hash]
+    #
+    # @api public
     def to_h
       KEYS.each_with_object({}) { |key, output| output[key] = send(key) }
     end
@@ -37,6 +51,7 @@ module AddThisEvent
       params.starts_at.strftime('%m/%d/%Y')
     end
 
+    # Event end date. Mandatory. Format: "MM/DD/YYYY"
     def end
       params.ends_at.strftime('%m/%d/%Y')
     end
@@ -46,6 +61,7 @@ module AddThisEvent
       params.starts_at.strftime('%I:%M:%S')
     end
 
+    # Event end time. Mandatory. Format: "HH/MM/SS"
     def endtime
       params.ends_at.strftime('%I:%M:%S')
     end
