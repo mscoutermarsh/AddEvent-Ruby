45285e47e1ab5faaca3a0c75af523254fd0be23b
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-09-27 18:03:26 -0700

    Fix options formatting documentation

--- lib/add_this_event.rb
+++ lib/add_this_event.rb
@@ -22,14 +22,33 @@ module AddThisEvent
   ##
   # Creates an AddThisEvent URL
   #
-  # @param [String] title The title of the event (this is what users will see on their calendar)
-  # @param [DateTime] starts_at
-  #   The <code>DateTime</code> of when the event begins.
-  # @param [DateTime] ends_at
-  #   The <code>DateTime</code> of when the event ends.
-  # @param [Hash] options Optional parameters for creating the event.
+  # == Usage
+  #  party_starts = DateTime.new(2016, 10, 2, 18, 0, 0, '+7')
+  #  party_ends = DateTime.new(2016, 10, 3, 3, 0, 0, '+7')
   #
-  # @overload wrap(options)
+  #  event_options = {
+  #    service: :google, # or 'google' works too!
+  #    organizer: 'Mike',
+  #    organizer_email: 'coutermarsh.mike@gmail.com',
+  #    alert: 15, # set alarm for 15 minutes before the party starts
+  #    description: "A little get together to celebrate DHH's birthday. BYORG (Bring your own ruby gems)",
+  #    location: "Ruby Bar, San Francisco CA"
+  #  }
+  #
+  #  url = AddThisEvent.url(title: "DHH's Birthday Bash", starts_at: party_starts, ends_at: party_ends, options: event_options)
+  #
+  # == Time Zones
+  # To keep things simple, this gem converts the <code>starts_at</code> and <code>ends_at</code>
+  # to UTC and sends all times to AddThisEvent as UTC. When the end user adds the event to their
+  # calendar, it will automatically be converted to their local timezone.
+  #
+  # @overload url(title, starts_at, ends_at, options)
+  #   @param [String] title The title of the event (this is what users will see on their calendar)
+  #   @param [DateTime] starts_at
+  #     The <code>DateTime</code> of when the event begins.
+  #   @param [DateTime] ends_at
+  #     The <code>DateTime</code> of when the event ends.
+  #   @param [Hash] options Optional parameters for creating the event.
   #   @option options [String] :client_id
   #     The API client id (https://addthisevent.com/account). Only need to include this
   #     if you did not set it using +config+.
@@ -49,27 +68,11 @@ module AddThisEvent
   #     Location of the Event. Ex: +'San Francisco, CA'+
   #   @option options [String] :reference
   #     Tracking parameter for analytics. Ex: <code>"iPhone"</code> or <code>"Newsletter Oct 2016"</code>
-  #   @option options [String] :template (default)
+  #   @option options [String] :template
   #     ID of a AddThisEvent template. Uses the default template if not defined.
   #     You can get this ID from the AddThisEvent dashboard after creating a template.
   #   @option options [Boolean] :all_day_event (false)
   #     Is it an all day event? True or False
-  #
-  # == Usage
-  #  party_starts = DateTime.new(2016, 10, 2, 18, 0, 0, '+7')
-  #  party_ends = DateTime.new(2016, 10, 3, 3, 0, 0, '+7')
-  #
-  #  event_options = {
-  #    service: :google, # or 'google' works too!
-  #    organizer: 'Mike',
-  #    organizer_email: 'coutermarsh.mike@gmail.com',
-  #    alert: 15, # set alarm for 15 minutes before the party starts
-  #    description: "A little get together to celebrate DHH's birthday. BYORG (Bring your own ruby gems)",
-  #    location: "Ruby Bar, San Francisco CA"
-  #  }
-  #
-  #  url = AddThisEvent.url(title: "DHH's Birthday Bash", starts_at: party_starts, ends_at: party_ends, options: event_options)
-  #
   #
   # @return [String] AddThisEvent Direct URL
   #
