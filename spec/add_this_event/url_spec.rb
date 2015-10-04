8fc7f1f556276ec9893ed41bfe2bdd6b64939704
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-10-04 10:16:29 -0700

    fix spec with new time

--- spec/add_this_event/url_spec.rb
+++ spec/add_this_event/url_spec.rb
@@ -16,8 +16,8 @@ describe AddThisEvent::Url do
     it 'generates an AddThisEvent url' do
       expect(subject).to eq 'https://addevent.to/dir/?all_day_event=false&client=fake-key' \
                             '&date_format=MM%2FDD%2FYYYY&end=10%2F06%2F1988&endext=AM' \
-                            '&endtime=12%3A00%3A00&start=10%2F06%2F1988&startext=AM' \
-                            '&starttime=12%3A00%3A00&timezone=Africa%2FCasablanca&title=Event%20Title'
+                            '&endtime=00%3A00%3A00&start=10%2F06%2F1988&startext=AM' \
+                            '&starttime=00%3A00%3A00&timezone=Africa%2FCasablanca&title=Event%20Title'
     end
   end
 end
